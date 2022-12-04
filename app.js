875c3b0f9eaaa224a5cacdb69613d8cd3347820a
Charan Raj Kandula <83225523+charanrajkandula@users.noreply.github.com>
2022-12-04 03:40:57 -0500

    developed student register page, login page and welcome dashboard

--- app.js
+++ app.js
@@ -1,48 +1,50 @@
-const express = require('express');
+// Setup server, session and middleware here.
+// Setup server, session and middleware here.
+const express = require("express");
 const app = express();
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const configRoutes = require('./routes');
+const session = require("express-session");
+const configRoutes = require("./routes");
 
-const myLogger = function (req, res, next) {
-    console.log(`[${new Date().toUTCString()} ]: ${req.method} ${req.originalUrl} ${  req.session.user ? '':'Non-' }Authenticated User`);
-    next();
-}
+const exphbs = require("express-handlebars");
 
+const static = express.static(__dirname + "/public");
+app.use("/public", static);
+
+//app.use("/public", static);
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-
-app.use(session({
-    name: 'AuthCookie',
-    secret: 'some secret string!',
+app.use(
+  session({
+    name: "AuthCookie",
+    secret: "some secret string!",
     resave: false,
     saveUninitialized: true,
-    cookie: {maxAge: 60000}
+  })
+);
 
-  }));
-
-app.use(myLogger);
-
-app.use('/protected', (req, res, next) => {
-  // console.log(req.session.id);
-  if (!req.session.user) {
-    // return res.redirect('/');
-    return res.status(403).render('forbiddenAccess');
-  } else {
-    next();
+//middleware
+const myLogger = function (req, res, next) {
+  auth = "(Non-Authenticated User)";
+  if (req.session.user) {
+    auth = "(Authenticated User)";
   }
-});
+  console.log(
+    "[" + new Date().toUTCString() + "]: ",
+    req.method,
+    req.originalUrl,
+    auth
+  );
+  next();
+};
+app.use(myLogger);
 
 configRoutes(app);
 
-
 app.listen(3000, () => {
   console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost:3000');
+  console.log("Your routes will be running on http://localhost:3000");
 });
--- data/users.js
+++ data/users.js
@@ -1,89 +1,262 @@
-const bcrypt = require('bcrypt');
-const mongoCollections = require('./../config/mongoCollections');
-const users = mongoCollections.user_collection;
-const {ObjectId} = require('mongodb');
+const mongoCollections = require("../config/mongoCollections");
+const users = mongoCollections.users;
+const { ObjectId } = require("mongodb");
+var bcrypt = require("bcryptjs");
+var salt = bcrypt.genSaltSync(10);
 
-const USERNAME_REGEX = /^[a-zA-Z0-9]{4,}$/;
-const PASSWORD_REGEX = /^(?=.*[A-Z])(?=.*\d)(?=.*([^a-zA-Z\d\s])).{6,}$/;
-
-const createUser = async (
-  username, password
-) => { 
-  //check for username exists.
-  let errMsg = '';
-
-  if (username.trim().length === 0)
-      return {errMsg : 'Username cannot be an empty string or just spaces'}
-  if (password.trim().length === 0)
-      return {errMsg : 'Password cannot be an empty string or just spaces'}
-  
-  if (!checkRegexFn(USERNAME_REGEX,username)) 
-    return {errMsg : 'Invalid username format'}
-  
-  if (!checkRegexFn(PASSWORD_REGEX,password)) 
-    return {errMsg : 'Invalid password format'}
-
-    const user_collection = await users();
-
-    const getUser = await user_collection.findOne({username: username.toLowerCase()});
-
-    if (getUser?.username === username.toLowerCase()) 
-      return {errMsg: 'A user with this username already exists'}
-
-    const hashedPwd = await bcrypt.hash(password,10);
-
-    let newUser = {
-      username: username.toLowerCase(),
-      password: hashedPwd
-    };
-
-    const insertInfo = await user_collection.insertOne(newUser);
-    if (!insertInfo.acknowledged || !insertInfo.insertedId)
-      return {code: 500 , errMsg:'Could not add User'};
-
-  // const testing = await bcrypt.compare(password,hashedPwd);
-  // console.log("BCRYPE COMPARE ", testing);
-
-  return {insertedUser: insertInfo.acknowledged,errMsg};
-
-};
-
-const checkUser = async (username, password) => { 
-  let errMsg = '';
-
-  if (username.trim().length === 0)
-      return {errMsg : 'Username cannot be an empty string or just spaces'}
-  if (password.trim().length === 0)
-      return {errMsg : 'Password cannot be an empty string or just spaces'}
-  
-  if (!checkRegexFn(USERNAME_REGEX,username)) 
-    return {errMsg : 'Invalid username format'}
-  
-  if (!checkRegexFn(PASSWORD_REGEX,password)) 
-    return {errMsg : 'Invalid password format'}
-
-  const user_collection = await users();
-
-  const getUser = await user_collection.findOne({username: username.toLowerCase()});
-
-  if(!getUser){
-    return {errMsg : "Either the username or password is invalid"}
-  }
-
-  const checkPassword = await bcrypt.compare(password,getUser?.password);
-
-  if(!checkPassword){
-    return {errMsg: "Either the username or password is invalid"}
-  }
-
-  return {authenticatedUser: !!getUser,errMsg};
-};
-
-
-const checkRegexFn = (regex_exp,input) => {
-
-  return regex_exp.test(input);
-
+//email validation
+function ValidateEmail(mail) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return mailformat.test(mail);
 }
 
-module.exports = {createUser,checkUser};
+//validate phonenumber
+// function phonenumber(inputtxt) {
+//   var phoneno = /^\d{10}$/;
+//   return /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/.test(inputtxt);
+
+// }
+
+//chech phone
+// function phonenumber(phone) {
+//   const regex = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+//   return regex.test(phone);
+//   //console.log(regex.test(phone))
+// }
+
+//space check
+function hasWhiteSpace(s) {
+  return s.indexOf(" ") >= 0;
+}
+//alpha numeric
+function onlyLettersAndNumbers(str) {
+  return /^[A-Za-z0-9]*$/.test(str);
+}
+//checking Password must have at least one uppercase character, one number and  one special character.
+function checkPassword(str) {
+  return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^\w\s]).{6,}$/.test(str);
+}
+
+const createUser = async (
+  firstnameData,
+  lastnameData,
+  usernameData,
+  emailData,
+  dobData,
+  genderData,
+  streetaddressData,
+  cityData,
+  zipcodeData,
+  country,
+  phonenumberData,
+  passwordData
+) => {
+  //validate data first
+  usernameData = usernameData.toLowerCase();
+  emailData = emailData.toLowerCase();
+  if (firstnameData.length <= 0) {
+    return {
+      validation_error: "The length of the first must be greater than 0! ",
+    };
+  }
+  if (lastnameData.length < 4) {
+    return {
+      validation_error: "You have entered an invalid phoone number! ",
+    };
+  }
+  if (usernameData.length < 4) {
+    return {
+      validation_error: "The length of the Username must be greater than 4 ",
+    };
+  }
+  if (hasWhiteSpace(usernameData)) {
+    return {
+      validation_error: "Username must not contain space character",
+    };
+  }
+  if (onlyLettersAndNumbers(usernameData) == false) {
+    return {
+      validation_error: "Username must be Alphanumeric",
+    };
+  }
+  if (checkPassword(passwordData) == false) {
+    return {
+      validation_error:
+        "Password must have at least one uppercase character, one number and  one special character.",
+    };
+  }
+  if (hasWhiteSpace(passwordData)) {
+    return {
+      validation_error: "Password must not contain space character",
+    };
+  }
+  if (passwordData.length < 6) {
+    return {
+      validation_error: "The length of the Password must be greater than 6 ",
+    };
+  }
+  if (ValidateEmail(emailData)) {
+    return {
+      validation_error: "You have entered an invalid email address! ",
+    };
+  }
+
+  // if (phonenumber(phonenumberData)) {
+  //   return {
+  //     validation_error: "You have entered an invalid phoone number! ",
+  //   };
+  // }
+
+  //check phone number
+
+  //verify user does not exist in db
+  doesUserExist = await getUserByUsername(usernameData);
+
+  doesPhoneExist = await getUserByPhone(phonenumberData);
+  doesEmailExist = await getUserByEmail(emailData);
+
+  if (doesPhoneExist != null) {
+    return {
+      validation_error:
+        "Please provide another Phone number, the given phone number  address already exits! ",
+    };
+  }
+
+  if (doesEmailExist != null) {
+    return {
+      validation_error:
+        "Please provide another email address, the given email address already exits! ",
+    };
+  }
+
+  if (doesUserExist == null) {
+    //user does not exist, therefore save record
+    let new_user;
+    new_user = {
+      firstnameData: firstnameData,
+      lastnameData: lastnameData,
+      usernameData: usernameData,
+      emailData: emailData,
+      dobData: dobData,
+      genderData: genderData,
+      streetaddressData: streetaddressData,
+      cityData: cityData,
+      zipcodeData: zipcodeData,
+      country: country,
+      phonenumberData: phonenumberData,
+      passwordData: bcrypt.hashSync(passwordData, salt),
+    };
+    console.log("new_user", new_user);
+
+    const userCollection = await users();
+    const insertInfo = await userCollection.insertOne(new_user);
+    if (!insertInfo.insertedId) throw "Could not add user";
+
+    const inserted_user = await getUserById(insertInfo.insertedId);
+    console.log("inserted_user", inserted_user);
+    return { inserted_user: true };
+  } else {
+    return { user_exists: true };
+  }
+
+  return { error: true };
+};
+
+const checkUser = async (username, password) => {
+  //if user finds in db, return true
+  username = username.toLowerCase();
+
+  if (username.length < 4) {
+    return {
+      validation_error: "The length of the Username must be greater than 4 ",
+    };
+  }
+  if (hasWhiteSpace(username)) {
+    return {
+      validation_error: "Username must not contain space character",
+    };
+  }
+  if (hasWhiteSpace(password)) {
+    return {
+      validation_error: "Password must not contain space character",
+    };
+  }
+  if (password.length < 6) {
+    return {
+      validation_error: "The length of the Password must be greater than 6 ",
+    };
+  }
+
+  var userCheck = await getUserByUsername(username);
+  if (userCheck == null) {
+    return {
+      validation_error: "Either the username or password is invalid",
+    };
+  } else {
+    //verify password
+    var pwdValid = bcrypt.compareSync(password, userCheck.passwordData); // true
+    if (pwdValid == true) {
+      //login success
+      return { authenticatedUser: true, data: userCheck };
+    } else {
+      //invalid password
+      return {
+        validation_error: "Either the username or password is invalid",
+      };
+    }
+  }
+};
+
+const checkEmail = async (email) => {
+  var emailCheck = await getUserByEmail(email);
+  if (emailCheck == null) {
+    return {
+      validation_error: "Email Id already exits, try with another email",
+    };
+  }
+};
+const checkPhone = async (phone) => {
+  var phoneCheck = await getUserByPhone(phone);
+  console.log("checkPhone", checkPhone);
+  if (phoneCheck == null) {
+    return {
+      validation_error:
+        "Phone Number already exits, try with another phone number",
+    };
+  }
+};
+
+//email adrees check from db
+const getUserByEmail = async (email) => {
+  const userCollection = await users();
+  const userEmail = await userCollection.findOne({ emailData: email });
+  console.log("getUserByEmail", userEmail);
+
+  return userEmail;
+};
+
+//phonenumber  check from db
+const getUserByPhone = async (phone) => {
+  const userCollection = await users();
+  const userPhone = await userCollection.findOne({ phonenumberData: phone });
+  console.log("getUserByPhone", userPhone);
+
+  return userPhone;
+};
+
+const getUserByUsername = async (username) => {
+  console.log("searching for " + username);
+  const userCollection = await users();
+  const user = await userCollection.findOne({ usernameData: username });
+  console.log("getUserByUsername", user);
+
+  return user;
+};
+
+const getUserById = async (id) => {
+  const userCollection = await users();
+  const userr_id = await userCollection.findOne({ _id: ObjectId(id) });
+  return userr_id;
+};
+
+module.exports = { createUser, checkUser };
